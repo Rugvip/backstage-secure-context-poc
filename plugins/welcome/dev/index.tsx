d0424eeff1533d31fe5e3b5fa6e63d509022c458
Patrik Oldsberg <poldsberg@gmail.com>
2020-05-21 20:14:48 +0200

    update prettier config

--- plugins/welcome/dev/index.tsx
+++ plugins/welcome/dev/index.tsx
@@ -1,4 +1,6 @@
 import { createDevApp } from '@backstage/dev-utils';
 import { plugin } from '../src/plugin';
 
-createDevApp().registerPlugin(plugin).render();
+createDevApp()
+  .registerPlugin(plugin)
+  .render();
