e24cfffa67e7f26e5d701cda0ed041c1fcdfff58
Patrik Oldsberg <poldsberg@gmail.com>
2020-05-22 17:25:42 +0200

    server: parse actions

--- packages/secure-context-server/src/server.ts
+++ packages/secure-context-server/src/server.ts
@@ -1,10 +1,37 @@
 import fs from 'fs-extra';
 import { Router as createRouter } from 'express';
 import { dirname, resolve as resolvePath } from 'path';
+import { transform } from 'sucrase';
 
 type Options = {
   plugins: string[];
 };
+
+type Action = {
+  id: string;
+  title: string;
+  body: string;
+  script: string;
+};
+
+export async function readAction(
+  dir: string,
+  filePath: string,
+): Promise<Action> {
+  const actionContent = await fs.readFile(resolvePath(dir, filePath), 'utf8');
+  const { code } = transform(actionContent, {
+    transforms: ['imports', 'typescript'],
+    production: true,
+    filePath,
+  });
+
+  return {
+    id: 'lol',
+    title: 'wut',
+    body: 'asd',
+    script: code,
+  };
+}
 
 export async function secureContextServer(options: Options) {
   const router = createRouter();
@@ -19,11 +46,15 @@ export async function secureContextServer(options: Options) {
       continue;
     }
 
-    const actions = await fs.readdir(secureActionsDir);
-
+    const actionNames = await fs.readdir(secureActionsDir);
     console.log(
-      `Found secure actions for ${pluginName}: ${actions.join(', ')}`,
+      `Found secure actions for ${pluginName}: ${actionNames.join(', ')}`,
     );
+
+    const actions = await Promise.all(
+      actionNames.map(filePath => readAction(secureActionsDir, filePath)),
+    );
+    console.log('DEBUG: actions =', actions);
   }
 
   return router;
